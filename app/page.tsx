2aac191afbcf3fa357f36e87748957c98e44e46f
Federico Jimenez <federico@Federicos-MacBook-Pro.local>
2023-08-25 15:45:36 -0400

    added more to about

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
         <div className="catch-phrase">
           <i className="fire">Firefighter</i>
           <b className="turned">=&gt;</b>
-          <i className="dev">&#x7B;Software.Developer()&#x7D;</i>
+          <i className="dev">&#x7B;Software.Developer&#x7D;</i>
         </div>
         <Image
           className="profile-pic md:w-96"
@@ -20,9 +20,13 @@ export default function Home() {
       <div className="about" id="contact">
         <h1 className="about-header">About</h1>
         <p className="about-description">
-          I&apos;m a former firefighter from New Orleans. Now living in NY. Huge
-          friends and Game of Thrones fan. Really enjoy watching movies. Blood
-          type is coffee. Fitness junkie. Amateur chess player.
+          How you doin? I&apos;m a former firefighter from New Orleans. Now
+          living in NY. Huge friends and Game of Thrones fan. Blood type is
+          coffee. Fitness junkie. Amateur chess player. Now I code. I know my
+          way around the front and back end of applications, full-stack. I enjoy
+          watching my code come to life on the web. I even get a kick from
+          solving bugs and figuring things out. Used to put out fires in houses,
+          now I'm putting them out in my code.
         </p>
       </div>
     </main>
