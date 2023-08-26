fa487cdd21eb91cca869e24c13124ca32a546911
Federico Jimenez <federico@Federicos-MacBook-Pro.local>
2023-08-25 20:26:13 -0400

    moved components to hero

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,10 +2,6 @@ import Navbar from "@/app/components/Navbar";
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter, Outfit } from "next/font/google";
-import dynamic from "next/dynamic";
-import Contact from "@/app/contact/page";
-import Resume from "@/app/resume/page";
-const Project = dynamic(() => import("@/app/projects/page"), { ssr: false });
 
 const inter = Inter({ subsets: ["latin"] });
 const outfit = Outfit({ subsets: ["latin"], weight: ["300", "400", "700"] });
@@ -24,9 +20,6 @@ export default function RootLayout({
       <body className={`${outfit.className} ${inter.className}`}>
         <Navbar />
         {children}
-        <Resume />
-        <Project />
-        <Contact />
       </body>
     </html>
   );
