4127db8d44e0febe584b638ea9cf11ed46a83f49
Federico Jimenez <federico@Federicos-MacBook-Pro.local>
2023-08-26 21:10:05 -0400

    fixed mobile bug

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,40 +1,66 @@
 "use client";
 import Link from "next/link";
-import Image from "next/image";
-import logo from "@/public/logo.png";
-import { AiOutlineMenu } from "react-icons/ai";
+import { useState } from "react";
+import { AiOutlineMenu, AiFillCloseSquare } from "react-icons/ai";
+import { BsChevronLeft, BsChevronRight, BsFire } from "react-icons/bs";
 
 export default function Navbar() {
+  const [active, setActive] = useState(false);
+
   return (
-    <header className="primary-header">
-      <nav className="navigation">
-        <input type="checkbox" id="check" />
-        <label htmlFor="check" className="checkbtn">
-          <AiOutlineMenu />
-        </label>
-        <Image className="logo" id="logo" src={logo} alt="logo img" />
-        <ul className="nav-list">
-          <li className="nav-item">
-            <Link className="link" style={{ color: "white" }} href="/">
-              About
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link className="link" style={{ color: "white" }} href="#resume">
-              Resume
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link className="link" style={{ color: "white" }} href="#project">
-              Projects
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link className="link" style={{ color: "white" }} href="#contact">
-              Contact Me
-            </Link>
-          </li>
-        </ul>
+    <header>
+      <nav className="w-full fixed z-10 bg-red-950 top-0 left-0 right-0">
+        <div className="justify-between px-4 mx-auto lg:max-w-7xl md:items-center md:flex md:px-8">
+          <div>
+            <div className="flex items-center justify-between py-3 md:py-5 md:block">
+              <div className="logo flex text-white md:text-4xl">
+                <BsChevronLeft className="logo-arrow" />
+                <BsFire className="logo-fire" />
+                <BsChevronRight className="logo-arrow" />
+              </div>
+              <div className="md:hidden">
+                <button
+                  onClick={() => setActive(!active)}
+                  className="p-2 text-white rounded-md outline-none focus:border-white focus:border"
+                >
+                  {active ? <AiFillCloseSquare /> : <AiOutlineMenu />}
+                </button>
+              </div>
+            </div>
+          </div>
+          <div>
+            <div
+              className={`flex-1 justify-self-center mt-8 md:block md:pb-0 md:mt-0 ${
+                active
+                  ? "border-t-2 pt-8 md:border-none md:h-fit md:p-0 block"
+                  : "hidden"
+              }`}
+            >
+              <ul className="h-screen md:h-auto justify-center md:flex">
+                <li className="pb-6 text-xl text-white py-2 md:px-6 text-left">
+                  <Link href="/" onClick={() => setActive(!active)}>
+                    Home
+                  </Link>
+                </li>
+                <li className="pb-6 text-xl text-white py-2 md:px-6 text-left">
+                  <Link href="#resume" onClick={() => setActive(!active)}>
+                    Resume
+                  </Link>
+                </li>
+                <li className="pb-6 text-xl text-white py-2 md:px-6 text-left">
+                  <Link href="#project" onClick={() => setActive(!active)}>
+                    Projects
+                  </Link>
+                </li>
+                <li className="pb-6 text-xl text-white py-2 md:px-6 text-left">
+                  <Link href="#contact" onClick={() => setActive(!active)}>
+                    Contact Me
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
       </nav>
     </header>
   );
