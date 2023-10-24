034bf69ce3a8bdc4d8d0dd858346b040a28244da
Federico Jimenez <federico@Federicos-MBP.lan>
2023-10-24 00:20:49 -0400

    removed form

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,75 +1,15 @@
-"use client";
 import {
   AiFillGithub,
   AiFillLinkedin,
   AiFillMail,
   AiFillPhone,
 } from "react-icons/ai";
-import submitContact from "@/app/libs/submit";
 
 export default function Contact() {
   return (
     <>
       <h1 className="linethrough font-bold text-2xl">CONTACT ME</h1>
       <div className="contact wrapper" id="contact">
-        <form action={submitContact}>
-          <div className="contact-form">
-            <div className="first-name">
-              <label htmlFor="firstName">First name</label>
-              <input
-                type="text"
-                id="firstName"
-                name="firstName"
-                placeholder="Jon"
-                required
-              />
-            </div>
-            <div className="last-name">
-              <label htmlFor="lastName">Last name</label>
-              <input
-                type="text"
-                id="lastName"
-                name="lastName"
-                placeholder="Snow"
-                required
-              />
-            </div>
-            <div className="phone">
-              <label htmlFor="phone">Phone number</label>
-              <input
-                type="tel"
-                id="phone"
-                name="phone"
-                placeholder="281-330-8004"
-                pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
-                required
-              />
-            </div>
-            <div className="email">
-              <label htmlFor="email">Email address</label>
-              <input
-                type="email"
-                id="email"
-                name="email"
-                placeholder="jon.snow@castleblack.com"
-                required
-              />
-            </div>
-          </div>
-          <div className="message">
-            <label htmlFor="message">Your message</label>
-            <textarea
-              id="message"
-              name="message"
-              rows={4}
-              placeholder="Write your thoughts here..."
-            ></textarea>
-          </div>
-          <button className="form-btn" type="submit">
-            Send Message
-          </button>
-        </form>
-
         <ul className="icon-list">
           <li className="icon-item">
             <a
